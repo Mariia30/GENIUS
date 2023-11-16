e4a95ae3917ec02a134238f52553fc10be78c34c
Mariia <levkovskaya.masha@gmail.com>
2023-11-16 13:47:54 +0200

    adaptive

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,3 +1,5 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
